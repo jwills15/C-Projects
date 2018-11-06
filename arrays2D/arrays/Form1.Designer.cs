860253574caa728006d457978af18b3da421a9a7
jwills15 <43358328+jwills15@users.noreply.github.com>
2018-11-05 18:04:29 -0800

    Add files via upload

---
+++ arrays2D/arrays/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace arrays
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.picFish = new System.Windows.Forms.PictureBox();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.picBox2 = new System.Windows.Forms.PictureBox();
+            this.picBox3 = new System.Windows.Forms.PictureBox();
+            this.picBox4 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.picBox8 = new System.Windows.Forms.PictureBox();
+            this.picBox7 = new System.Windows.Forms.PictureBox();
+            this.picBox6 = new System.Windows.Forms.PictureBox();
+            this.picBox5 = new System.Windows.Forms.PictureBox();
+            this.picBox12 = new System.Windows.Forms.PictureBox();
+            this.picBox11 = new System.Windows.Forms.PictureBox();
+            this.picBox10 = new System.Windows.Forms.PictureBox();
+            this.picBox9 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picFish)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picFish
+            // 
+            this.picFish.Image = ((System.Drawing.Image)(resources.GetObject("picFish.Image")));
+            this.picFish.Location = new System.Drawing.Point(97, 342);
+            this.picFish.Name = "picFish";
+            this.picFish.Size = new System.Drawing.Size(78, 47);
+            this.picFish.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picFish.TabIndex = 0;
+            this.picFish.TabStop = false;
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(207, 342);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(97, 47);
+            this.btnMove.TabIndex = 1;
+            this.btnMove.Text = "Move";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // picBox1
+            // 
+            this.picBox1.Location = new System.Drawing.Point(12, 12);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(125, 75);
+            this.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox1.TabIndex = 2;
+            this.picBox1.TabStop = false;
+            this.picBox1.UseWaitCursor = true;
+            // 
+            // picBox2
+            // 
+            this.picBox2.Location = new System.Drawing.Point(179, 12);
+            this.picBox2.Name = "picBox2";
+            this.picBox2.Size = new System.Drawing.Size(125, 75);
+            this.picBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox2.TabIndex = 3;
+            this.picBox2.TabStop = false;
+            this.picBox2.UseWaitCursor = true;
+            // 
+            // picBox3
+            // 
+            this.picBox3.Location = new System.Drawing.Point(345, 12);
+            this.picBox3.Name = "picBox3";
+            this.picBox3.Size = new System.Drawing.Size(125, 75);
+            this.picBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox3.TabIndex = 4;
+            this.picBox3.TabStop = false;
+            this.picBox3.UseWaitCursor = true;
+            // 
+            // picBox4
+            // 
+            this.picBox4.Location = new System.Drawing.Point(519, 12);
+            this.picBox4.Name = "picBox4";
+            this.picBox4.Size = new System.Drawing.Size(125, 75);
+            this.picBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox4.TabIndex = 5;
+            this.picBox4.TabStop = false;
+            this.picBox4.UseWaitCursor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.Location = new System.Drawing.Point(345, 342);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(97, 47);
+            this.btnAuto.TabIndex = 6;
+            this.btnAuto.Text = "Auto";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.btnAuto_Click);
+            // 
+            // picBox8
+            // 
+            this.picBox8.Location = new System.Drawing.Point(519, 111);
+            this.picBox8.Name = "picBox8";
+            this.picBox8.Size = new System.Drawing.Size(125, 75);
+            this.picBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox8.TabIndex = 10;
+            this.picBox8.TabStop = false;
+            this.picBox8.UseWaitCursor = true;
+            // 
+            // picBox7
+            // 
+            this.picBox7.Location = new System.Drawing.Point(345, 111);
+            this.picBox7.Name = "picBox7";
+            this.picBox7.Size = new System.Drawing.Size(125, 75);
+            this.picBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox7.TabIndex = 9;
+            this.picBox7.TabStop = false;
+            this.picBox7.UseWaitCursor = true;
+            // 
+            // picBox6
+            // 
+            this.picBox6.Location = new System.Drawing.Point(179, 111);
+            this.picBox6.Name = "picBox6";
+            this.picBox6.Size = new System.Drawing.Size(125, 75);
+            this.picBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox6.TabIndex = 8;
+            this.picBox6.TabStop = false;
+            this.picBox6.UseWaitCursor = true;
+            // 
+            // picBox5
+            // 
+            this.picBox5.Location = new System.Drawing.Point(12, 111);
+            this.picBox5.Name = "picBox5";
+            this.picBox5.Size = new System.Drawing.Size(125, 75);
+            this.picBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox5.TabIndex = 7;
+            this.picBox5.TabStop = false;
+            this.picBox5.UseWaitCursor = true;
+            // 
+            // picBox12
+            // 
+            this.picBox12.Location = new System.Drawing.Point(519, 214);
+            this.picBox12.Name = "picBox12";
+            this.picBox12.Size = new System.Drawing.Size(125, 75);
+            this.picBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox12.TabIndex = 14;
+            this.picBox12.TabStop = false;
+            this.picBox12.UseWaitCursor = true;
+            // 
+            // picBox11
+            // 
+            this.picBox11.Location = new System.Drawing.Point(345, 214);
+            this.picBox11.Name = "picBox11";
+            this.picBox11.Size = new System.Drawing.Size(125, 75);
+            this.picBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox11.TabIndex = 13;
+            this.picBox11.TabStop = false;
+            this.picBox11.UseWaitCursor = true;
+            // 
+            // picBox10
+            // 
+            this.picBox10.Location = new System.Drawing.Point(179, 214);
+            this.picBox10.Name = "picBox10";
+            this.picBox10.Size = new System.Drawing.Size(125, 75);
+            this.picBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox10.TabIndex = 12;
+            this.picBox10.TabStop = false;
+            this.picBox10.UseWaitCursor = true;
+            // 
+            // picBox9
+            // 
+            this.picBox9.Location = new System.Drawing.Point(12, 214);
+            this.picBox9.Name = "picBox9";
+            this.picBox9.Size = new System.Drawing.Size(125, 75);
+            this.picBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox9.TabIndex = 11;
+            this.picBox9.TabStop = false;
+            this.picBox9.UseWaitCursor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(656, 401);
+            this.Controls.Add(this.picBox12);
+            this.Controls.Add(this.picBox11);
+            this.Controls.Add(this.picBox10);
+            this.Controls.Add(this.picBox9);
+            this.Controls.Add(this.picBox8);
+            this.Controls.Add(this.picBox7);
+            this.Controls.Add(this.picBox6);
+            this.Controls.Add(this.picBox5);
+            this.Controls.Add(this.btnAuto);
+            this.Controls.Add(this.picBox4);
+            this.Controls.Add(this.picBox3);
+            this.Controls.Add(this.picBox2);
+            this.Controls.Add(this.picBox1);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.picFish);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmMain";
+            this.Text = "How to use Arrays";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picFish)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox9)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picFish;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.PictureBox picBox2;
+        private System.Windows.Forms.PictureBox picBox3;
+        private System.Windows.Forms.PictureBox picBox4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.PictureBox picBox8;
+        private System.Windows.Forms.PictureBox picBox7;
+        private System.Windows.Forms.PictureBox picBox6;
+        private System.Windows.Forms.PictureBox picBox5;
+        private System.Windows.Forms.PictureBox picBox12;
+        private System.Windows.Forms.PictureBox picBox11;
+        private System.Windows.Forms.PictureBox picBox10;
+        private System.Windows.Forms.PictureBox picBox9;
+    }
+}
+
---
+++ arrays2D/arrays/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arrays
+{
+    public partial class frmMain : Form
+    {
+        //declare our global variables
+        //declare an array to organize our picture boxes
+        //declare a 2D array
+        PictureBox[,] theTank = new PictureBox[3, 4];
+        int fishRow = 0;
+        int fishCol = 0;
+        System.Random r = new System.Random((int)System.DateTime.Now.Ticks);
+
+        //create a booleen switch
+        Boolean onOff = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            //this will randomly move the fish to some picture box
+            theTank[fishRow, fishCol].Image = null;
+            fishCol = r.Next(0, 4);
+            fishRow = r.Next(0, 3);
+            theTank[fishRow, fishCol].Image = picFish.Image;
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //this code runs one time at start up
+
+            //row 1
+            theTank[0, 0] = picBox1;
+            theTank[0, 1] = picBox2;
+            theTank[0, 2] = picBox3;
+            theTank[0, 3] = picBox4;
+            //row 2
+            theTank[1, 0] = picBox5;
+            theTank[1, 1] = picBox6;
+            theTank[1, 2] = picBox7;
+            theTank[1, 3] = picBox8;
+            //row 3
+            theTank[2, 0] = picBox9;
+            theTank[2, 1] = picBox10;
+            theTank[2, 2] = picBox11;
+            theTank[2, 3] = picBox12;
+
+            int randomIntegerCol = r.Next(0, 4);
+            int randomIntegerRow = r.Next(0, 3);
+            theTank[randomIntegerRow, randomIntegerCol].Image = picFish.Image;
+            fishRow = randomIntegerRow;
+            fishCol = randomIntegerCol;
+
+        }
+
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            //this moves the fish automatically
+            if (onOff == false)
+            {
+                timer1.Enabled = true;
+                onOff = true;
+            }
+            else if (onOff == true)
+            {
+                timer1.Enabled = false;
+                onOff = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //this the move code
+            btnMove_Click(sender, e);
+        }
+    }
+}
